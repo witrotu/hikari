2608ee5da51e9b44be83d44d430cf2a05ccc0379
witrotu <44129299+witrotu@users.noreply.github.com>
2022-08-18 13:43:16 +0700

    1.0
    start

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: '_site/',
+	globPatterns: [
+		'**/*.{html,ico,xml,png,json,jpg,txt,ttf,svg,js}'
+	],
+	swDest: '_site/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
